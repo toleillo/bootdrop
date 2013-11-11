669da00910d91cf6c3d6b95f247cbb8cb83bddae
Juan Olalla <juan.olalla@gmail.com>
2013-11-11 12:42:54 +0100

    Add default.settings.local.php

---
+++ shared/default.settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'database_name',
+  'username' => 'root',
+  'password' => '',
+  'host' => 'localhost',
+  'prefix' => '',
+  'collation' => 'utf8_general_ci',
+);
